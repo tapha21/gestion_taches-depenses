111caa7e392d68f72ad5b9a045fc7cbec6aa164b
Tapha21 <tvphvtall@gmail.com>
2026-01-15 22:18:24 +0000

    Version Beta 1.0

--- src/main/java/com/app/gestion_taches_depenses/dtos/request/InscriptionRequestDto.java
+++ src/main/java/com/app/gestion_taches_depenses/dtos/request/InscriptionRequestDto.java
@@ -4,7 +4,9 @@ public class InscriptionRequestDto {
     private String nom;
     private String email;
     private String motDePasse;
+    private double solde = 0; // initialisation à 0
 
+    // ===== Getters & Setters =====
     public String getNom() { return nom; }
     public void setNom(String nom) { this.nom = nom; }
 
@@ -13,4 +15,7 @@ public class InscriptionRequestDto {
 
     public String getMotDePasse() { return motDePasse; }
     public void setMotDePasse(String motDePasse) { this.motDePasse = motDePasse; }
+
+    public double getSolde() { return solde; }
+    public void setSolde(double solde) { this.solde = solde; }
 }
