80a20caa30d2a8f75cfbd100b3260d8cb56ce6b2
Tapha21 <tvphvtall@gmail.com>
2026-01-15 00:09:42 +0000

    Version 1.1

--- src/main/java/com/app/gestion_taches_depenses/config/CorsConfig.java
+++ src/main/java/com/app/gestion_taches_depenses/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:4200",
-                                "https://sama-gestion.vercel.app"
+                                "https://sama-gestions.vercel.app"
                                 
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
